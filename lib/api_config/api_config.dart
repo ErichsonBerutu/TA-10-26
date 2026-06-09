ac0d02f307c28336a87dcee151c403a7fe38eaf3
samuel11422026 <samuelaritonang1st@gmail.com>
2026-06-09 07:53:55 +0700

    06/09/2026

--- lib/api_config/api_config.dart
+++ lib/api_config/api_config.dart
@@ -5,7 +5,7 @@ class ApiConfig {
     if (Platform.isAndroid) {
       // Menggunakan localhost karena port 8000 telah di-reverse via ADB (adb reverse tcp:8000 tcp:8000)
       // Ini menjamin koneksi lancar tanpa terhalang Windows Defender Firewall atau isolasi Wi-Fi.
-      return "http://172.27.69.11:8000/api";
+      return "http://172.27.69.109:8000/api";
     } else if (Platform.isIOS) {
       // Simulator iOS di laptop bisa langsung membaca localhost
       return "http://localhost:8000/api";
