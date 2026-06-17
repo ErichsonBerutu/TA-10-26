2d09ae9ea643eeeca2549ac8d5aee901a4380762
ErichsonBerutu <rezanuelberutu@gmail.com>
2026-06-17 08:51:12 +0700

    push changes

--- lib/api_config/api_config.dart
+++ lib/api_config/api_config.dart
@@ -4,7 +4,7 @@ class ApiConfig {
   static String get baseUrl {
     if (Platform.isAndroid) {
       // Menggunakan IP lokal laptop yang aktif saat ini agar HP Android fisik dapat terhubung
-      return "http://10.42.181.25:8000/api";
+      return "http://172.27.80.230:8000/api";
     } else if (Platform.isIOS) {
       // Simulator iOS di laptop bisa langsung membaca localhost
       return "http://localhost:8000/api";
